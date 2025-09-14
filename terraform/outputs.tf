2f3d03383db2be6a16494a261328480717e468a6
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-14 16:25:12 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,15 @@ output "aws_region_debug" {
 output "aws_identity_debug" {
   value = data.aws_caller_identity.current.arn
 }
+
+output "role_name" {
+  value = data.aws_iam_role.external_secrets_role.name
+}
+
+output "role_arn" {
+  value = data.aws_iam_role.external_secrets_role.arn
+}
+
+output "datalake_policy_arn" {
+  value = aws_iam_policy.datalake_policy.arn
+}
