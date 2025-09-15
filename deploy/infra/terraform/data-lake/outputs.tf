7fb28780ea078de40a325ecaf384bae26064bf36
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-15 17:31:37 +0530

    Delete deploy/infra/terraform/data-lake directory

--- deploy/infra/terraform/data-lake/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bucket_name" { value = aws_s3_bucket.datalake.bucket }
-output "s3_arn" { value = aws_s3_bucket.datalake.arn }
-output "glue_db" { value = aws_glue_catalog_database.bankapp_db.name }
