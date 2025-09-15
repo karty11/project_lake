806ac3bf83ef0365261b6d6d9657913c15dfbddb
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-15 17:30:44 +0530

    Delete terraform/infra/terraform/data-lake directory

--- terraform/infra/terraform/data-lake/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "region" { type = string, default = "us-west-2" }
-variable "environment" { type = string, default = "dev" }
-variable "bucket_name" { type = string, default = "bankapp-datalake-${var.environment}" }
-variable "allow_glue" { type = bool, default = false }
