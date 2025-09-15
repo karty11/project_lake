36583574b83a638cf9dd40944260aa07c8dbcc6a
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-15 18:15:52 +0530

    Update variables.tf

--- infra/terraform/data-lake/variables.tf
+++ infra/terraform/data-lake/variables.tf
@@ -1,4 +1,16 @@
-variable "region" { type = string, default = "us-west-2" }
-variable "environment" { type = string, default = "dev" }
-variable "bucket_name" { type = string, default = "bankapp-datalake-${var.environment}" }
-variable "allow_glue" { type = bool, default = false }
+variable "region" { 
+type = string
+default = "us-west-2" 
+}
+variable "environment" { 
+type = string 
+default = "dev" 
+}
+variable "bucket_name" { 
+type = string
+default = "bankapp-datalake-dev"
+}
+variable "allow_glue" {
+type = bool
+default = false 
+}
