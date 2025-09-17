46ec73c4883c30387def0c434dfc2280930acb2f
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-18 01:08:05 +0530

    Update output.tf

--- infra/terraform/data-lake/output.tf
+++ infra/terraform/data-lake/output.tf
@@ -9,3 +9,11 @@ output "s3_arn" {
 output "glue_db" {
   value = length(aws_glue_catalog_database.bankapp_db) > 0 ? aws_glue_catalog_database.bankapp_db[0].name : ""
 }
+
+output "glue_job_name" {
+  value = aws_glue_job.mysql_to_s3.name
+}
+
+output "glue_crawler_name" {
+  value = aws_glue_crawler.mysql_export_crawler.name
+}
