3ae83fcb653bf51f7d5120f1d4d6aaba9e80b353
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-18 10:53:54 +0530

    Update glue-job.tf

--- infra/terraform/data-lake/glue-job.tf
+++ infra/terraform/data-lake/glue-job.tf
@@ -1,101 +1,99 @@
-#############################
-# IAM Role for Glue Job
-#############################
-resource "aws_iam_role" "glue_role" {
-  name = "bankapp-glue-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "glue.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+data "aws_caller_identity" "current" {}
+
+resource "aws_glue_catalog_database" "datalake_db" {
+  name = "datalake_db"
 }
-
-resource "aws_iam_role_policy_attachment" "glue_policy_attach" {
-  role       = aws_iam_role.glue_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-}
-
-# Allow Glue to access Secrets Manager + S3 bucket
 resource "aws_iam_policy" "glue_extra_policy" {
-  name        = "bankapp-glue-extra-policy"
-  description = "Allow Glue Job to access S3 datalake bucket + Secrets Manager"
+  name        = "glue-extra-policy"
+  description = "Extra permissions for Glue job"
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow",
+        Effect   = "Allow"
         Action   = [
-          "secretsmanager:GetSecretValue"
-        ],
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
         Resource = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:bankapp/mysql*"
       },
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
           "s3:ListBucket"
-        ],
+        ]
         Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.datalake_bucket.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.datalake_bucket.bucket}/*"
+          "arn:aws:s3:::${var.datalake_bucket}",
+          "arn:aws:s3:::${var.datalake_bucket}/*"
         ]
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "glue_extra_policy_attach" {
-  role       = aws_iam_role.glue_role.name
-  policy_arn = aws_iam_policy.glue_extra_policy.arn
-}
-
-#############################
-# Glue Job (MySQL → S3 export)
-#############################
+############################################
+# Glue Job to export MySQL data to S3
+############################################
 resource "aws_glue_job" "mysql_to_s3" {
-  name     = "mysql-to-s3-export"
-  role_arn = aws_iam_role.glue_role.arn
+  name     = "mysql-to-s3"
+  role_arn = var.glue_role_arn
 
   command {
     name            = "glueetl"
     python_version  = "3"
-    script_location = "s3://${aws_s3_bucket.datalake_bucket.bucket}/scripts/mysql_to_s3.py"
+    script_location = "s3://${var.datalake_bucket}/scripts/mysql_to_s3.py"
   }
 
   default_arguments = {
-    "--TempDir"             = "s3://${aws_s3_bucket.datalake_bucket.bucket}/tmp/"
+    "--TempDir"             = "s3://${var.datalake_bucket}/tmp/"
+    "--output_path"         = "s3://${var.datalake_bucket}/bankapp/transactions/"
     "--job-bookmark-option" = "job-bookmark-enable"
-    "--secret_name"         = "bankapp/mysql"
-    "--output_path"         = "s3://${aws_s3_bucket.datalake_bucket.bucket}/bankapp/transactions/"
   }
 
-  glue_version = "4.0"
-  max_capacity = 2
+  glue_version = "3.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
 }
 
-#############################
-# Glue Crawler
-#############################
+############################################
+# Glue crawler for exported data
+############################################
 resource "aws_glue_crawler" "mysql_export_crawler" {
-  name          = "mysql-to-s3-crawler"
-  role          = aws_iam_role.glue_role.arn
+  name         = "mysql-export-crawler"
+  role         = var.glue_role_arn
   database_name = aws_glue_catalog_database.datalake_db.name
 
   s3_target {
-    path = "s3://${aws_s3_bucket.datalake_bucket.bucket}/bankapp/transactions/"
+    path = "s3://${var.datalake_bucket}/bankapp/transactions/"
   }
 
-  schedule = "cron(0 * * * ? *)" # run hourly
+  schedule = "cron(0 * * * ? *)" # every hour
 }
+
+
+
+
+############################################
+# Data lookups for existing infra
+############################################
+data "aws_security_group" "glue_sg" {
+  filter {
+    name   = "group-name"
+    values = ["glue-sg"] # change to your SG name
+  }
+  vpc_id = var.vpc_id
+}
+
+data "aws_subnet" "private_subnet1" {
+  filter {
+    name   = "tag:Name"
+    values = ["private-subnet-1"] # change to your subnet tag
+  }
+}
+
 
